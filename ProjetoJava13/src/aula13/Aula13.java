b083ddbe8de6477b4ea04a22d861c4352e9b0f3b
jwictor <39440505+jwictor@users.noreply.github.com>
2018-05-19 15:04:35 -0300

    Add files via upload

---
+++ ProjetoJava13/src/aula13/Aula13.java
@@ -0,0 +1,16 @@
+
+package aula13;
+
+
+public class Aula13 {
+
+    
+    public static void main(String[] args) {
+       //Programa Principal
+       Lobo x = new Lobo();
+       x.emitirSom();
+       
+       
+    }
+    
+}
---
+++ ProjetoJava13/src/aula13/Cachorro.java
@@ -0,0 +1,30 @@
+
+package aula13;
+
+
+public class Cachorro extends Lobo{
+    @Override
+            public void emitirSom(){
+    System.out.println("Au Au Au Au");
+            }
+            public void reagir(String frase){
+                if("Toma comida".equals(frase) );{
+                System.out.println("Abanar e latir");
+            
+                    System.out.println("Rosnar");
+                    }
+                    
+                    
+            }     
+            
+            public void reagir(int hora, int min){
+                
+            }
+            public void reagir(boolean dono){
+                
+            }
+            public void reagir(int idade, float peso){
+                
+            }
+            
+}
